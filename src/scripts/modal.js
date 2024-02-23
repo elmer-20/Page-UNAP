8f6e4046f6659705a8265879e7b1bdd53425cd13
Anghelo Flores <anghelogfp@gmail.com>
2024-02-23 18:36:28 -0500

    ReultadosExtraordinario2024

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -31,7 +31,7 @@ let span = document.getElementsByClassName("close")[0];
 // Cuando se hace clic en el botón, abrir el modal
 btn.onclick = function() {
   modal.style.display = "block";
-  modalImg.src = "/images/ficha1.jpeg"; // Reemplaza "imagen.jpg" con la ruta de tu imagen
+  modalImg.src = "/src/images/ficha1.jpeg"; // Reemplaza "imagen.jpg" con la ruta de tu imagen
 }
 
 // Cuando se hace clic en <span> (x), cerrar el modal
@@ -40,8 +40,32 @@ span.onclick = function() {
 }
 
 // Cuando el usuario hace clic en cualquier parte fuera del modal, cerrarlo
+
+
+// Obtener elementos del DOM
+let modal1 = document.getElementById('myModal1');
+let modalImg1 = document.getElementById("img011");
+let btn1 = document.getElementById("openModalBtn1");
+let span1 = document.getElementsByClassName("close1")[0];
+
+// Cuando se hace clic en el botón, abrir el modal
+btn1.onclick = function() {
+  modal1.style.display = "block";
+  modalImg1.src = "/src/images/ficha2.jpeg"; // Reemplaza "imagen.jpg" con la ruta de tu imagen
+}
+
+// Cuando se hace clic en <span> (x), cerrar el modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// Cuando el usuario hace clic en cualquier parte fuera del modal, cerrarlo
+
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  } else if (event.target == modal1) {
+    modal1.style.display = "none";
   }
 }
